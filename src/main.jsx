7011ef493db072f64de33ce1d1a5744c2edc225d
Koby <kobysac@gmail.com>
2024-01-02 23:19:16 -0800

    added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 // import 'normalize.css';
 // import 'reset-css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.jsx';
+import Error from './pages/Error/Error.jsx';
+import Contact from './pages/Contact/Contact.jsx';
+import AboutMe from './pages/AboutMe/AboutMe.jsx';
+import Portfolio from './pages/Portfolio/Portfolio.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Portfolio />,
+      },
+      {
+        path: '/About',
+        element: <AboutMe />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
