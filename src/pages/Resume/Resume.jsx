62eb613c6ecfdd99bc1edd71118e3eae426c5517
Koby <kobysac@gmail.com>
2024-01-06 13:16:43 -0800

    added content to resume

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,11 +1,30 @@
 import './Resume.css';
 
+import realResume from "../../assets/Resume-JanV1-24.pdf";
+import resumePic from "../../assets/resume.png";
+
 const Resume = ({ }) => {
 
 
   return (
-    <div className='mt-4 mt-md-5'>
-      <h1>Resume</h1>
+    <div className='container mt-4'>
+      <div className='row justify-content-center mx-2'>
+
+        <h2 className='text-center'>Resume</h2>
+
+        <div>
+          <div className="card mx-auto" style={{ maxWidth: '500px' }}>
+            {/* <div className="card"> */}
+            <img src={resumePic} className="card-img-top blur" alt="Resume" />
+            <div className="card-body d-flex flex-column align-items-center">
+              <button href="#" className="btn btn-primary" style={{ maxWidth: '10em' }}>Download</button>
+              <p className="card-text mt-1">Last Updated: 01/06/2024</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
