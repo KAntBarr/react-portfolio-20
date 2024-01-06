d9effc092bf55aa67ba5736bd1b7ce711dcda3ed
Koby <kobysac@gmail.com>
2024-01-05 23:59:30 -0800

    added some project pics

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,7 +1,10 @@
 import './Portfolio.css';
 import Project from "../../components/Project/Project.jsx";
 
-import seniorProject from "../../assets/senior_project_rc-car.jpg"
+import seniorProject from "../../assets/senior_project_rc-car.jpg";
+import weatherDashboard from "../../assets/weather_dashboard.png";
+import drinkMovieCombo from "../../assets/drink_movie_combo.jpg";
+import geeksGarage from "../../assets/geeks_garage.png";
 
 const Portfolio = ({ }) => {
 
@@ -19,28 +22,28 @@ const Portfolio = ({ }) => {
               cardText={'Self Driving RC Car'}
             />
             <Project
+              imageAlt={'Weather Dashboard'}
+              imageSrc={weatherDashboard}
+              cardText={'Weather Dashboard'}
+            />
+            <Project
+              imageAlt={'Drink Movie Combo'}
+              imageSrc={drinkMovieCombo}
+              cardText={'Drink Movie Combo'}
+            />
+            <Project
+              imageAlt={'Geeks Garage'}
+              imageSrc={geeksGarage}
+              cardText={'Geeks Garage'}
+            />
+            <Project
               imageAlt={'test'}
-              imageSrc={}
+              imageSrc={''}
               cardText={'test'}
             />
             <Project
               imageAlt={'test'}
-              imageSrc={}
-              cardText={'test'}
-            />
-            <Project
-              imageAlt={'test'}
-              imageSrc={}
-              cardText={'test'}
-            />
-            <Project
-              imageAlt={'test'}
-              imageSrc={}
-              cardText={'test'}
-            />
-            <Project
-              imageAlt={'test'}
-              imageSrc={}
+              imageSrc={''}
               cardText={'test'}
             />
             {/* </div> */}
