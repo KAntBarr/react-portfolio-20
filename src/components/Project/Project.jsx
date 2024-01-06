5fe30ba1223db4a455772de4d8b4054ac0c173ee
Koby <kobysac@gmail.com>
2024-01-06 14:00:12 -0800

    working on img hovering

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,19 +1,45 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Project.css';
 
 const Project = ({ imageSrc, imageAlt, cardText }) => {
+  const [isHovered, setIsHovered] = useState(false);
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
-    // <div className="card" style= {{width: '18rem'}}>
-    //   <img src={imageSrc} className="card-img-top" alt={imageAlt} />
-    //   <div className="card-body">
-    //     <p className="card-text">{cardText}</p>
-    //   </div>
-    // </div>
     <div className='col-12 col-md-6 col-lg-4 mb-3'>
       <div className='d-flex justify-content-center'>
         <div className="card" style={{ width: '18rem' }}>
-          <img src={imageSrc} className="card-img-top" alt={imageAlt} />
+          <div className="image-container"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+            <img
+              src={imageSrc}
+              className="card-img-top"
+              alt={imageAlt}
+              style={{
+                height: 'auto', // Maintain aspect ratio
+                filter: isHovered ? 'brightness(150%)' : 'none', // Apply brightness filter on hover
+              }}
+            />
+            <div className="text-overlay">
+              {isHovered && <Link
+                to="https://www.google.com"
+                target='_blank'
+                className=""
+              >
+                {imageAlt}
+              </Link>}
+            </div>
+          </div>
+
           <div className="card-body">
             <p className="card-text">{cardText}</p>
           </div>
