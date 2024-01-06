1e91b946c483e5a9ff1f31b5608d0afbab729d88
Koby <kobysac@gmail.com>
2024-01-06 00:44:26 -0800

    portfolio projects looking good

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,10 +1,14 @@
 import './Portfolio.css';
 import Project from "../../components/Project/Project.jsx";
 
-import seniorProject from "../../assets/senior_project_rc-car.jpg";
+// import seniorProject from "../../assets/senior_project_rc-car.jpg";
+// import seniorProject from "../../assets/senior_rc-car.png";
+import seniorProject from "../../assets/senior_rc-cars.png";
 import weatherDashboard from "../../assets/weather_dashboard.png";
 import drinkMovieCombo from "../../assets/drink_movie_combo.jpg";
 import geeksGarage from "../../assets/geeks_garage.png";
+import techBlog from "../../assets/tech_blog.png";
+import quizSite from "../../assets/quiz_site.png";
 
 const Portfolio = ({ }) => {
 
@@ -13,13 +17,13 @@ const Portfolio = ({ }) => {
     <>
       <div className='container'>
         <h2 className='my-5'>Look at my portfolio</h2>
-        <div className='row'>
+        <div className='row align-items-center'>
           {/* <div className='col '> */}
             {/* <div className='d-flex justify-content-center'> */}
             <Project
-              imageAlt={'Self Driving RC Car'}
-              imageSrc={seniorProject}
-              cardText={'Self Driving RC Car'}
+              imageAlt={'Tech Blog'}
+              imageSrc={techBlog}
+              cardText={'Tech Blog'}
             />
             <Project
               imageAlt={'Weather Dashboard'}
@@ -32,19 +36,19 @@ const Portfolio = ({ }) => {
               cardText={'Drink Movie Combo'}
             />
             <Project
+              imageAlt={'Self Driving RC Car'}
+              imageSrc={seniorProject}
+              cardText={'Self Driving RC Car'}
+            />
+            <Project
+              imageAlt={'Quiz Site'}
+              imageSrc={quizSite}
+              cardText={'Quiz Site'}
+            />
+            <Project
               imageAlt={'Geeks Garage'}
               imageSrc={geeksGarage}
               cardText={'Geeks Garage'}
-            />
-            <Project
-              imageAlt={'test'}
-              imageSrc={''}
-              cardText={'test'}
-            />
-            <Project
-              imageAlt={'test'}
-              imageSrc={''}
-              cardText={'test'}
             />
             {/* </div> */}
           {/* </div> */}
