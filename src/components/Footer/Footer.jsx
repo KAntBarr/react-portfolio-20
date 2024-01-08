fe9e255025d3f66a37c954975b7911aad5a4796d
Koby <kobysac@gmail.com>
2024-01-07 23:19:48 -0800

    added input verification to contact form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,10 +10,12 @@ const Footer = ({ }) => {
           {/* <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
             <svg className="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
           </a> */}
-          <span className="mb-3 mb-sm-0 text-body-secondary">© 2024 Koby's Box, Pending...</span>
+          <span className="mb-1 mb-sm-0 text-body-secondary">© 2024 Koby's Box, Pending...</span>
         </div>
 
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <span className="mb-3 mb-sm-0 text-body-secondary mx-5 mx-sm-3">Created Using React</span>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex mx-5 mx-sm-0">
           <li className="ms-3"><a className="text-body-secondary" target="_blank" href="https://github.com/KAntBarr"><svg
             height="32"
             width="32"
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,15 +3,23 @@ import './Contact.css';
 
 const Contact = ({ }) => {
 
+  const nameRegex = /^[A-Za-z]+(?:[ -][A-Za-z]+)*$/;
+  const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
   const [isName, setName] = useState(false);
   const [isEmail, setEmail] = useState(false);
   const [isMessage, setMessage] = useState(false);
   const [isValid, setValid] = useState(false);
+  const [isNew, setNew] = useState(true);
 
-  // I think this updates only on the first page/state load
   useEffect(() => {
+    if (!isName && !isEmail && !isMessage && !isNew) { // work on submit button logic
+      setValid(true);
+    } else {
+      setValid(false);
+    }
     
-  }, [])
+  }, [isName, isEmail, isMessage])
 
   // I think this might update after every state change
   useEffect(() => {
@@ -19,15 +27,35 @@ const Contact = ({ }) => {
   });
 
   const handleName = (event) => {
-    console.log(event.target);
+    const name = event.target.value.trim();
+    if (nameRegex.test(name)) {
+      setName(false);
+      // console.log("setting to false");
+    } else {
+      setName(true);
+      // console.log("setting to true");
+    }
+    setNew(false);
   }
 
   const handleEmail = (event) => {
-    console.log(event.target);
+    const email = event.target.value.trim();
+    if (emailRegex.test(email)) {
+      setEmail(false);
+    } else {
+      setEmail(true);
+    }
+    setNew(false);
   }
 
   const handleMessage = (event) => {
-    console.log(event.target);
+    const message = event.target.value.trim();
+    if (message.length < 1) {
+      setMessage(true);
+    } else {
+      setMessage(false);
+    }
+    setNew(false);
   }
 
   const handleSubmission = (event) => {
@@ -46,18 +74,18 @@ const Contact = ({ }) => {
               <label htmlFor="inputName" className="form-label text-start">Name</label>
               <input type="name" className="form-control" id="inputName" onBlur={handleName}
               aria-describedby="nameHelp" />
-              <div id="nameHelp" className="form-text text-start">A name must be provided</div>
+              {isName && <div id="nameHelp" className="form-text text-start">A name must be provided</div>}
             </div>
             <div className="mb-3 d-flex flex-column">
               <label htmlFor="inputEmail" className="form-label text-start">Email address</label>
               <input type="email" className="form-control" id="inputEmail" onBlur={handleEmail} aria-describedby="emailHelp" />
-              <div id="emailHelp" className="form-text text-start">A valid email must be provided</div>
+              {isEmail && <div id="emailHelp" className="form-text text-start">A valid email must be provided</div>}
             </div>
             <div className="mb-3 d-flex flex-column">
               <label htmlFor="inputMessage" className="form-label text-start">Message</label>
               <textarea type="message" className="form-control" id="inputMessage" onBlur={handleMessage} rows='4'
               aria-describedby="messageHelp" />
-              <div id="messageHelp" className="form-text text-start">Input must be provided</div>
+              {isMessage && <div id="messageHelp" className="form-text text-start">Input must be provided</div>}
             </div>
             {isValid 
             ? 
