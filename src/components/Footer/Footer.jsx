eb1bb423a23d34b168eb097a64915b766d34b568
Koby <kobysac@gmail.com>
2024-01-07 22:34:05 -0800

    working on contact form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = ({ }) => {
           {/* <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
             <svg className="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
           </a> */}
-          <span className="mb-3 mb-md-0 text-body-secondary">© 2024 Koby's World, Pending...</span>
+          <span className="mb-3 mb-sm-0 text-body-secondary">© 2024 Koby's Box, Pending...</span>
         </div>
 
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,9 +3,10 @@ import './Contact.css';
 
 const Contact = ({ }) => {
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [isName, setName] = useState(false);
+  const [isEmail, setEmail] = useState(false);
+  const [isMessage, setMessage] = useState(false);
+  const [isValid, setValid] = useState(false);
 
   // I think this updates only on the first page/state load
   useEffect(() => {
@@ -17,30 +18,58 @@ const Contact = ({ }) => {
 
   });
 
+  const handleName = (event) => {
+    console.log(event.target);
+  }
+
+  const handleEmail = (event) => {
+    console.log(event.target);
+  }
+
+  const handleMessage = (event) => {
+    console.log(event.target);
+  }
+
+  const handleSubmission = (event) => {
+    event.preventDefault();
+    console.log('oh poops');
+  }
+
+
   return (
     <div className='container text-center'>
       <div className='row'>
         <div className='col-md-8 offset-md-2 col-lg-6 offset-lg-3'>
-          <form className='mt-4 mt-md-5 d-flex flex-column mx-3 flex-grow-1'>
+          <form className='mt-4 mt-md-5 d-flex flex-column mx-3 flex-grow-1' onSubmit={handleSubmission}>
             <h1>Contact Me</h1>
             <div className="mb-3 d-flex flex-column">
-              <label htmlFor="exampleInputName1" className="form-label text-start">Name</label>
-              <input type="name" className="form-control" id="exampleInputName1" />
+              <label htmlFor="inputName" className="form-label text-start">Name</label>
+              <input type="name" className="form-control" id="inputName" onBlur={handleName}
+              aria-describedby="nameHelp" />
+              <div id="nameHelp" className="form-text text-start">A name must be provided</div>
             </div>
             <div className="mb-3 d-flex flex-column">
-              <label htmlFor="exampleInputEmail1" className="form-label text-start">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-              {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+              <label htmlFor="inputEmail" className="form-label text-start">Email address</label>
+              <input type="email" className="form-control" id="inputEmail" onBlur={handleEmail} aria-describedby="emailHelp" />
+              <div id="emailHelp" className="form-text text-start">A valid email must be provided</div>
             </div>
             <div className="mb-3 d-flex flex-column">
-              <label htmlFor="exampleInputMessage1" className="form-label text-start">Message</label>
-              <textarea type="message" className="form-control" id="exampleInputMessage1" rows='4' />
+              <label htmlFor="inputMessage" className="form-label text-start">Message</label>
+              <textarea type="message" className="form-control" id="inputMessage" onBlur={handleMessage} rows='4'
+              aria-describedby="messageHelp" />
+              <div id="messageHelp" className="form-text text-start">Input must be provided</div>
             </div>
-            {/* <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-      </div> */}
-            <button type="submit" className="btn btn-primary mx-auto px-4">Submit</button>
+            {isValid 
+            ? 
+            <button 
+            type="submit" 
+            className="btn btn-primary mx-auto px-4">Submit</button> 
+            : 
+            <button 
+            type="submit" 
+            className="btn btn-primary mx-auto px-4" disabled>Submit</button>
+            }
+            <div id="messageHelp" className="form-text mt-3">Thank you for submitting a message</div>
           </form>
         </div>
       </div>
