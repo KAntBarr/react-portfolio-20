8455ab8791d566cc130e10079e2a96c9000192e3
Koby <kobysac@gmail.com>
2024-02-03 22:44:44 -0800

    finished 'about me' and changed some project's pics/links

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -19,11 +19,11 @@ const AboutMe = ({ }) => {
           </p>
           <h2 className='text-md-start mt-2 mb-3 bordered-text'>Past Education</h2>
           <p className='text-md-start ptext'>
-            My past education includes...
+            I started learning programming by taking a course that only dealt with flow charts and pseudocode at Sac State. It wasn't until the next semester that I took an intro course where I started using my first programming language, Java. Java was used in most of my beginning upperdivision courses: Data Structures & Algorithms, Object Oriented & Graphics Programming, and Computer Theory. C was introduced to me in an embedded systems course, and was later primarily used in my intro and advanced operating systems courses. The advanced OS course had us build out an operating system in phases. This was my favorite course by far, I felt like I truly understood what was going on and would have loved to continue building that project. Maybe one day in the future, I'll build my own OS again. Just recently, I completed an online full-stack coding bootcamp through UC Berkeley Extension. It was an amazing experience, even as tiring as it may have been. Some of the most important things we learned were Javascript, React, RESTful/GraphQL APIs, SQL/NoSQL, Bootstrap and Material UI, Redux, etc. I don't feel that I truly got to master these topics due to the pace of the bootcamp, but I would love to continue learning more.
           </p>
           <h2 className='text-md-start mt-2 mb-3 bordered-text'>Future Developments</h2>
           <p className='text-md-start ptext'>
-            My plans for the future include...
+            My plans for the foreseeable future are to continue building myself up until I become a Software Engineer. This includes grinding leetcode, with a goal of one problem a day, and, to do an online self-paced course through Udacity to build my Java programming skills. To also improve my skills in my current position and also in general, I will be completing Google's Coursera's IT Professional certification. Following that I would like to obtain my CCNA and have this all done by the end of this year(2024). Maybe after completing the Udacity course, I'll look into using C for embedded systems/OS projects. We'll see!
           </p>
         </div>
       </div>
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -8,8 +8,9 @@ import seniorProject from "../../assets/senior_rc-cars.png";
 import weatherDashboard from "../../assets/weather_dashboard.png";
 import drinkMovieCombo from "../../assets/drink_movie_combo.jpg";
 import geeksGarage from "../../assets/geeks_garage.png";
-import techBlog from "../../assets/tech_blog.png";
+// import techBlog from "../../assets/tech_blog.png";
 import quizSite from "../../assets/quiz_site.png";
+import socialMedia from "../../assets/social_media_clone_y.png";
 
 
 const Portfolio = ({ }) => {
@@ -21,63 +22,71 @@ const Portfolio = ({ }) => {
         <h1 className='mt-4 mt-md-5 mb-4 bordered-text'>Past Learning Experiences</h1>
         <div className='row align-items-center'>
           {/* <div className='col '> */}
-            {/* <div className='d-flex justify-content-center'> */}
-            <Project
+          {/* <div className='d-flex justify-content-center'> */}
+          {/* <Project
               imageSrc={techBlog}
               imageAlt={'Tech Blog'}
               cardText={'Tech Blog'}
               projectURL={'https://tech-blog-ucb-92de467f20ab.herokuapp.com'}
               githubURL={'https://github.com/KAntBarr/tech-blog-14'}
               projectSummaryNum={0}
-            />
-            <Project
-              imageSrc={weatherDashboard}
-              imageAlt={'Weather Dashboard'}
-              cardText={'Weather Dashboard'}
-              projectURL={'https://kantbarr.github.io/weather-dashboard-6'}
-              githubURL={'https://github.com/KAntBarr/weather-dashboard-6'}
-              projectSummaryNum={1}
-            />
-            <Project
-              imageSrc={drinkMovieCombo}
-              imageAlt={'Drink Movie Combo'}
-              cardText={'Drink Movie Combo'}
-              projectURL={'https://ucb-allstars-project-1.github.io/movie-cocktail-combo'}
-              githubURL={'https://github.com/ucb-allstars-project-1/movie-cocktail-combo'}
-              projectSummaryNum={2}
-            />
-            <Project
-              imageSrc={seniorProject}
-              imageAlt={'Self Driving RC Car'}
-              cardText={'Self Driving RC Car'}
-              projectURL={'https://www.youtube.com/playlist?list=PLHFOvcVOvE2IlpS0oUwSvtJbdVLczmRdj'}
-              githubURL={'https://github.com/csus-cpe190-191-team2/Team2'}
-              projectSummaryNum={3}
-            />
-            <Project
-              imageSrc={quizSite}
-              imageAlt={'Quiz Site'}
-              cardText={'Quiz Site'}
-              projectURL={'https://kantbarr.github.io/js-quiz-site-4'}
-              githubURL={'https://github.com/KAntBarr/js-quiz-site-4'}
-              projectSummaryNum={4}
-            />
-            <Project
-              imageSrc={geeksGarage}
-              imageAlt={'Geeks Garage'}
-              cardText={'Geeks Garage'}
-              projectURL={'https://geeks-garage-alpha-4b07057e044e.herokuapp.com/'}
-              githubURL={'https://github.com/ucb-geeks-garage-p2/geeks-garage'}
-              projectSummaryNum={5}
-            />
-            {/* </div> */}
+            /> */}
+          <Project
+            imageSrc={socialMedia}
+            imageAlt={'Social Media Clone'}
+            cardText={'Social Media Clone'}
+            projectURL={'https://social-media-clone-akmd.onrender.com'}
+            githubURL={'https://github.com/ucb-space-y-p3/social-media-clone-y'}
+            projectSummaryNum={0}
+          />
+          <Project
+            imageSrc={weatherDashboard}
+            imageAlt={'Weather Dashboard'}
+            cardText={'Weather Dashboard'}
+            projectURL={'https://kantbarr.github.io/weather-dashboard-6'}
+            githubURL={'https://github.com/KAntBarr/weather-dashboard-6'}
+            projectSummaryNum={1}
+          />
+          <Project
+            imageSrc={drinkMovieCombo}
+            imageAlt={'Drink Movie Combo'}
+            cardText={'Drink Movie Combo'}
+            projectURL={'https://ucb-allstars-project-1.github.io/movie-cocktail-combo'}
+            githubURL={'https://github.com/ucb-allstars-project-1/movie-cocktail-combo'}
+            projectSummaryNum={2}
+          />
+          <Project
+            imageSrc={seniorProject}
+            imageAlt={'Self Driving RC Car'}
+            cardText={'Self Driving RC Car'}
+            projectURL={'https://www.youtube.com/playlist?list=PLHFOvcVOvE2IlpS0oUwSvtJbdVLczmRdj'}
+            githubURL={'https://github.com/csus-cpe190-191-team2/Team2'}
+            projectSummaryNum={3}
+          />
+          <Project
+            imageSrc={quizSite}
+            imageAlt={'Quiz Site'}
+            cardText={'Quiz Site'}
+            projectURL={'https://kantbarr.github.io/js-quiz-site-4'}
+            githubURL={'https://github.com/KAntBarr/js-quiz-site-4'}
+            projectSummaryNum={4}
+          />
+          <Project
+            imageSrc={geeksGarage}
+            imageAlt={'Geeks Garage'}
+            cardText={'Geeks Garage'}
+            projectURL={'https://geeks-garage-alpha-4b07057e044e.herokuapp.com/'}
+            githubURL={'https://github.com/ucb-geeks-garage-p2/geeks-garage'}
+            projectSummaryNum={5}
+          />
+          {/* </div> */}
           {/* </div> */}
         </div>
       </div>
 
 
-      <Modal 
-        
+      <Modal
+
       />
     </>
   );
